1ac995bd99a5a36e093fe3770c63147872e17c74
virusdefender <qduliyang@outlook.com>
2014-05-18 21:03:24 +0800

    change quotes

--- probe.js
+++ probe.js
@@ -63,9 +63,9 @@ function json2str(o) {
 	var arr = [];
 	var fmt = function(s) {
 		if (typeof s == 'object' && s != null) return json2str(s);
-		return /^(string|number)$/.test(typeof s) ? "'" + s + "'" : s;
+		return /^(string|number)$/.test(typeof s) ? '"' + s + '"' : s;
 	}
-	for (var i in o) arr.push("'" + i + "':" + fmt(o[i]));
+	for (var i in o) arr.push('"' + i + '":' + fmt(o[i]));
 	return '{' + arr.join(',') + '}';
 } 
 
